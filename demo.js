bdad3f2d7533d28d2bd38c6b3d2121733ab0fbc1
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-01-09 15:13:29 +0530

    done

--- demo.js
+++ demo.js
@@ -1,2 +1,11 @@
-console.log("hi...")
-console.log("hi...")
+//variable declaration
+var a = 12 //int number
+var b = 32.43 //flot number
+var name = "Ashish." //string variable
+
+//a=54
+console.log(a+b)
+
+console.log(b)
+name="Sam"
+console.log(name)
