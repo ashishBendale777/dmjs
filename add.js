33f388f994b04f0b4f2a59701a2d24dda7d79eaa
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-01-10 15:07:07 +0530

    add

---
+++ add.js
@@ -0,0 +1,14 @@
+var a = 15
+var b = 7
+var ans = a + b
+console.log("Add",ans)
+//console.log(a+b)
+
+var minans = a - b
+console.log("Sub",minans)
+
+var divans = a / b
+console.log("Div",divans)
+
+var modans = a % b
+console.log("Mod",modans)
