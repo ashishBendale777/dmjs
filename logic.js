a28cc440cd17a8b533b13af593aabc816d1a3b23
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-01-10 15:32:36 +0530

    logic

---
+++ logic.js
@@ -0,0 +1,15 @@
+var a = 24
+var b = 21
+console.log(a > b)
+console.log(a == b)
+console.log(a <= b)
+console.log(a != b)
+console.log(a >= b && a <= b)
+console.log(a >= b || a <= b)
+
+//controll instruction
+if (a < b) {
+    console.log('A is greater')
+} else {
+    console.log("B is greater")
+}
