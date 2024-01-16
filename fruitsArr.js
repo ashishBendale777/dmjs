4da1b8c86b2ccd76a2ee3d66296aae1bb56b6a75
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-01-16 14:49:13 +0530

    arr

---
+++ fruitsArr.js
@@ -0,0 +1,27 @@
+
+//fruits array
+var fruits = ["Apple","Mango","Grapes"]
+
+//push add element at last index of an array
+fruits.push("Cherry")
+fruits.push("Banana")
+console.log("Before pop",fruits)
+
+//rmove element at last index from an array
+fruits.pop()
+fruits.pop()
+console.log("After Pop",fruits)
+
+console.log("Before Shift",fruits)
+
+//rmove element at first index from an array
+fruits.shift()
+console.log("After Shift",fruits)
+
+
+console.log("Before UnShift",fruits)
+//insert/add element at first index
+fruits.unshift("Kiwi")
+console.log("After UnShift",fruits)
+
+
