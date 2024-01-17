eec81d6612f0b5f35d2223aa736e230a0068ba9c
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-01-17 16:04:20 +0530

    done

---
+++ loops.js
@@ -0,0 +1,14 @@
+console.log("hi..")
+
+var num=54
+// == in increment opearater 
+//increment value by1
+
+//post-increment
+console.log(num++)//55
+console.log(++num)//56
+console.log(++num)//57
+console.log(num++)//57
+console.log(++num)//59
+console.log(num++)//
+console.log(--num)//58
