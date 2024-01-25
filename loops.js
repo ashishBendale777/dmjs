bce9693a689d3492142df0a52c67e6677d8ba3a8
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-01-25 15:38:40 +0530

    done

--- loops.js
+++ loops.js
@@ -4,6 +4,6 @@
 
 //const nums = [43, 65, 23, 76, 223, 76, 23, 98, 23, 76, 35, 88, 33, 85]
 const nums = [43, 65, 23, 76, 223, 34, 54]
-for (let index = 0; index < nums.length; index++) {
+for (let index = 0; index < nums.length ; index++) {
     console.log(nums[index])
 }
---
+++ strexm.js
@@ -0,0 +1,26 @@
+//String function
+
+//declare a string
+const data = "I am a Boy"
+
+console.log(data.toUpperCase())
+
+//charAt
+console.log(data.charAt(0))//o/p ==I
+
+//nreaks a string by charecter
+const words = data.split(' ')
+console.log(words)
+
+const otherString = ' and indian'
+console.log(data.concat(otherString))
+
+console.log(data.replace('Boy', 'Girl'))
+
+console.log(data.length)
+
+console.log(data.slice(2,8))
+
+console.log(data.indexOf('B'))
+
+
